40e42ec9a21cc41573c101ed44cc4a1ff7f9287e
yourmaingotoguy <Khalid.N.Mohamed@outlook.com>
2019-08-24 19:37:15 -0400

    Login UI Complete

---
+++ WPFLoginSample/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLoginSample
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Login_Dialog(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Move_Dialog(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
